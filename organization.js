1ba37471f1b6c29ba7e9386f87fa282094bf9d6b
Rachel Levy <57646446+rwillowlevy@users.noreply.github.com>
2020-05-13 21:21:57 -0500

    Create organization.js

---
+++ organization.js
@@ -0,0 +1,40 @@
+module.exports = function (sequelize, DataTypes) {
+    const organization = sequelize.define('organization', {
+
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+
+        organizationName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+            },
+        
+
+        websiteUrl: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+            },
+        
+
+        country: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+            },
+
+        otherInquiries: {
+            type: DataTypes.STRING,
+            allowNull: true
+            },
+
+        })
+    return User
+}
